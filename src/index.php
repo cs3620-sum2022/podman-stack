32dba1fcbda2e65231ea8b7428c348fba18426c1
Don B. Stringham <me@donbstringham.com>
2022-05-18 20:35:45 -0600

    Squashed some bugs!

--- src/index.php
+++ src/index.php
@@ -9,14 +9,15 @@ $pass = 'letmein';
 $charset = 'utf8';
 $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
 
+echo "Attempting to connect to database...<br />";
+
 try {
-    // $conn = mysqli_connect($host, $user, $pass, $dbname);
-    $pdo = new PDO($dsn, $user, $pass);
-    echo 'Database connected successfully';
+    $conn = mysqli_connect($host, $user, $pass, $dbname);
+    // $pdo = new PDO($dsn, $user, $pass);
+    echo 'Database connected successfully!';
     echo '<br />';
 }
-// catch (mysqli_sql_exception $e) {
-catch (\PDOException $e) {
+catch (mysqli_sql_exception $e) { // catch (\PDOException $e) {
     echo $e->getMessage() . " " . $e->getCode();
     echo '<br />';
 }
